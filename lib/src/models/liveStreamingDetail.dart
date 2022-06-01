ce13f27ed928fd15f4663ad6f48500c029acd703
tiresomenist <tiresomenist@naver.com>
2022-06-01 23:38:12 +0900

    라이브 시청자 오류 고친버전(그외는 아직 미구현)

--- lib/src/models/liveStreamingDetail.dart
+++ lib/src/models/liveStreamingDetail.dart
@@ -10,24 +10,16 @@ String liveStreamingDetailToJson(LiveStreamingDetail data) => json.encode(data.t
 
 class LiveStreamingDetail {
   LiveStreamingDetail({
-    this.actualStartTime,
-    this.scheduledStartTime,
     this.concurrentViewers,
   });
 
-  DateTime? actualStartTime;
-  DateTime? scheduledStartTime;
   String? concurrentViewers;
 
   factory LiveStreamingDetail.fromJson(Map<String, dynamic> json) => LiveStreamingDetail(
-    actualStartTime: DateTime.parse(json["actualStartTime"]),
-    scheduledStartTime: DateTime.parse(json["scheduledStartTime"]),
     concurrentViewers: json["concurrentViewers"],
   );
 
   Map<String, dynamic> toJson() => {
-    "actualStartTime": actualStartTime?.toIso8601String(),
-    "scheduledStartTime": scheduledStartTime?.toIso8601String(),
     "concurrentViewers": concurrentViewers,
   };
 }
